f7c1f4e55725264112a380840e3a318c6e495997
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-09-24 01:11:31 -0300

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,54 @@
+local RunService: RunService = game:GetService("RunService")
+
+local ScriptBinding = {}
+ScriptBinding.__index = ScriptBinding
+
+local BindingId: number = 0
+
+function ScriptBinding:IsBinded()
+	return self._binded == true
+end
+
+function ScriptBinding:Unbind()
+	if self._binded == false then
+		return
+	end
+
+	self._binded = false
+	RunService:UnbindFromRenderStep(
+		self._bindingId
+	)
+end
+
+export type ScriptBinding = typeof(
+	setmetatable({}, ScriptBinding)
+)
+
+return function(
+	priority: number,
+	callback: (deltaTime: number) -> ()
+)
+	assert(
+		typeof(priority) == 'number',
+		"Must be number"
+	)
+
+	assert(
+		typeof(callback) == 'function',
+		"Must be function"
+	)
+
+	BindingId += 1
+	local bindingId = "BindToRenderStep_".. BindingId
+
+	RunService:BindToRenderStep(
+		bindingId,
+		priority,
+		callback
+	)
+
+	return setmetatable({
+		_binded = true,
+		_bindingId = bindingId
+	}, ScriptBinding)
+end
